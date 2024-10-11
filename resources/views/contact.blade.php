3d59f26703f807d73e7abe64fc54de568bd19b10
JensV72 <jens.verhoeff@gmail.com>
2024-10-11 09:25:16 +0200

    Contactcontroller test

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<div>
+    Naam: {{$name}} Verhoeff
+    Leeftijd: 23
+    Geslacht: M
+{{--    school: {{$company}}--}}
+</div>
+
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact/{name}',[ContactController::class, 'name'])->name('contact');
 
 Route::get('/dashboard', function () {
     return view('dashboard');

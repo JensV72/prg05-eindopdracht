96b51937cf1c38c2b7b8bfbe0329138e5f8f3529
JensV72 <jens.verhoeff@gmail.com>
2024-10-16 16:16:14 +0200

    Footer aangepast en Changelog bijgewerkt

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,26 +4,37 @@
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                <img src="public/images/Team_fainort_logo.png" class="h-8" alt="logo team fainort" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Fainort</span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                    <a href="#" class="hover:underline me-4 md:me-6">Home</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    <a href="#" class="hover:underline me-4 md:me-6">Team</a>
                 </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
+                @if(Route::has('login'))
+                    @auth
+                        <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">Dashboard</a>
+                        </li>
+                    @else
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">Log in</a>
+                        </li>
+                        @if(Route::has('register'))
+                            <li>
+                                <a href="#" class="hover:underline">Register</a>
+                            </li>
+                        @endif
+                    @endauth
+                @endif
+
             </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="{{route("home")}}" class="hover:underline">Fainort™</a>. All Rights Reserved.</span>
     </div>
 </footer>
 
